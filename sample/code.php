6442663bf7618bc614b6c47b0ad7bc591c68f947
USAMI Kenta <tadsan@zonu.me>
2017-02-05 23:48:48 +0900

    Modify sample/code.php

--- sample/code.php
+++ sample/code.php
@@ -15,21 +15,38 @@ namespace Emacs\PhanEl;
 
 error_reporting(0);
 
-$dt = my_create_datetime();
+$dt = my_create_datetime([], $foo);
 assert($dt instanceof \DateTime);
 $x = $dt->setDate(2112, 9, 3);
 var_dump($dt, $x);
+$uid = getUserId([] + []);
+
+deprecated();
 
 /**
+ * @param  int $num
  * @return \DataTimeInterface
  */
-function my_create_datetime()
+function my_create_datetime($num)
 {
     if (false) {
         $v = [1, 2, 3];
-        $x = 1 + $v;
+        $x = 1 + $v + $z;
+        $w = split(",", "1,2,3");
+        echo HOGE . FooBar::Foo;
+        FooBar\Buz(getUserId());
+
         return $v;
     }
 
     return new \DateTimeImmutable;
 }
+
+/** @deprecated */
+function obsolete() {}
+
+/**
+ * @param  Hoge $name
+ * @return int
+ */
+function getUserId($string) { return [1]; }
